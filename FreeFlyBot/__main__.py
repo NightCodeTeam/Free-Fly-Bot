5e4602885aeb66e1980b42fcd9d04a4f6e3e82cd
Maxim Bodrov <activer12@mail.ru>
2024-03-27 01:31:19 +0300

    {feat:} tr

--- FreeFlyBot/__main__.py
+++ FreeFlyBot/__main__.py
@@ -1,7 +1,17 @@
+from pprint import pprint
+import discord
 
-def main():
-    pass
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
 
+    async def on_message(self, message):
+        #pprint(f'{message}')
+        print(f'{message.guild}\n{message.channel}\nMessage from {message.author.nick}: {message.content}')
 
-if __name__ == '__main__':
-    main()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+client.run('token')
