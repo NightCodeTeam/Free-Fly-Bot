8845f3e4248f2e83c12ddd833b10dfa1c9d2f6c7
iogannes <ckrab228@gmail.com>
2024-03-28 18:36:36 +0300

    fix: fixed conct names in settings

--- FreeFlyBot/settings.py
+++ FreeFlyBot/settings.py
@@ -2,12 +2,12 @@ from typing import Final
 
 
 ENV_FILE: Final = ".env"
-
-EVENTS: Final = ""
-TYPES: Final = ""
-DS_SERVERS: Final = ""
-PILOTS: Final = ""
-PILOT_ROLES: Final = ""
+#______названия таблиц в базе данных
+EVENTS_TABLE_NAME: Final = ""
+TYPES_TABLE_NAME: Final = ""
+DS_SERVERS_TABLE_NAME: Final = ""
+PILOTS_TABLE_NAME: Final = ""
+PILOT_ROLES_TABLE_NAME: Final = ""
 DB_NAME: Final = ""
 
 
