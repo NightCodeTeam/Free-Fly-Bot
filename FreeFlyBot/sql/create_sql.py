47af9198ceb5f36bdaa3a4d9c50899b3feefada0
Maxim Bodrov <activer12@mail.ru>
2024-03-28 18:45:40 +0300

    feat: python create sql

---
+++ FreeFlyBot/sql/create_sql.py
@@ -0,0 +1,23 @@
+import sqlite3
+from core import create_log
+from settings import (
+    SQL_BD_NAME,
+    CREATE_TABLE_EVENTS,
+    CREATE_TABLE_SERVERS,
+    CREATE_TABLE_TYPES,
+)
+
+
+def create_bd():
+    try:
+        with sqlite3.connect(SQL_BD_NAME) as db:
+            cursor = db.cursor()
+            cursor.execute(CREATE_TABLE_EVENTS)
+            db.commit()
+            cursor.execute(CREATE_TABLE_SERVERS)
+            db.commit()
+            cursor.execute(CREATE_TABLE_TYPES)
+            db.commit()
+            cursor.close()
+    except sqlite3.Error as error:
+        create_log(error, 'error')
