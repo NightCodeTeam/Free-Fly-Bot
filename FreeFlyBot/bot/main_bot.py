d608ca07436a754dfb51f578ca80a7c341905a83
MaxBro <activer12@mail.ru>
2024-03-28 09:09:22 +0300

    feat: bot base commands

--- FreeFlyBot/bot/main_bot.py
+++ FreeFlyBot/bot/main_bot.py
@@ -12,11 +12,6 @@ class Bot(commands.Bot):
     async def on_ready(self):
         print(f"Logged on as {self.user}!\n----")
 
-    # async def on_message(self, message):
-    #    if message.author != self.user:
-    #        print(
-    #            f"---\n{message.guild}\n{message.channel}\n{message.author.nick}: {message.content}"
-    #        )
 
 
 bot = Bot()
@@ -25,3 +20,52 @@ bot = Bot()
 @bot.command(name="helpme")
 async def helpme(ctx: commands.Context):
     await ctx.reply("Нет :(")
+
+
+@bot.command(name="eventlist", brief='This is the brief description', description='This is the full description')
+async def eventlist(ctx: commands.Context):
+    await ctx.reply('ЫЫЫ')
+
+ 
+
+@bot.command(name="addevent")
+async def addevent(ctx: commands.Context):
+    pass
+
+
+
+@bot.command(name="updateevent")
+async def updateevent(ctx: commands.Context):
+    pass
+
+
+@bot.command(name="deleteevent")
+async def deleteevent(ctx: commands.Context):
+    pass
+
+
+@bot.command(name="types")
+async def types(ctx: commands.Context):
+    pass
+
+
+@bot.command(name="addtype")
+async def addtype(ctx: commands.Context):
+    pass
+
+
+                    
+@bot.command(name="deletetype")
+async def deletetype(ctx: commands.Context):
+    pass
+
+
+@bot.command(name="test2")
+async def test2(ctx: commands.Context):
+    pass
+
+
+@bot.command(name="test1")
+async def test(ctx: commands.Context):
+    pass
+
--- FreeFlyBot/core/__init__.py
+++ FreeFlyBot/core/__init__.py
@@ -1,3 +1,2 @@
 from .debug import create_log, logd
 from .dot_env import get_env, update_env
-from .singleton import Singleton
