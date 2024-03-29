44f1a6fe3a22b3853c744ec7bb5cbb60c41a80b2
iogannes <ckrab228@gmail.com>
2024-03-30 01:18:45 +0300

    fix!!: поменял зазвание везде!

--- FreeFlyBot/sql/__init__.py
+++ FreeFlyBot/sql/__init__.py
@@ -7,7 +7,7 @@ from .data_classes import (
 )
 from .create_sql import create_bd
 from .server_query import (db_add_server,
-                           db_check_for_exist,
+                           db_check_server_for_exist,
                            db_delete_server, 
                            db_server_list) # возвращает список объектов класса DiscordServer  ну или пустой список(
 from .event_query import db_add_event, db_delete_event, db_get_events
--- FreeFlyBot/sql/server_query.py
+++ FreeFlyBot/sql/server_query.py
@@ -19,7 +19,7 @@ async def db_add_server(data: DiscordServer):
         await db.commit()
         
 
-async def db_check_for_exist(server_id: int) -> bool: #True если такая запись уже есть!!11
+async def db_check_server_for_exist(server_id: int) -> bool: #True если такая запись уже есть!!11
     async with aiosqlite.connect(SQL_BD_NAME) as db: #!!!!!эээ а почему у нас везде await db.execute а тут async with?!!?
         async with db.execute(f"""SELECT EXISTS (SELECT server_id FROM {DS_SERVERS_TABLE_NAME} WHERE server_id = {server_id});""") as cursor: 
             return True if list(await cursor.fetchall())[0][0] == 1 else False
