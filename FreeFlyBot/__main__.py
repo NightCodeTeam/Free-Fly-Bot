e9bb380947ae04b7af14d404fc13f7bad6fd25c8
Maxim Bodrov <activer12@mail.ru>
2024-04-04 20:26:19 +0300

    feat: timer? raw test

--- FreeFlyBot/__main__.py
+++ FreeFlyBot/__main__.py
@@ -1,4 +1,6 @@
 from sys import argv
+from threading import Thread
+
 from core import create_log, get_env, update_env, Timer
 from sql import create_bd
 from bot import Bot
@@ -8,13 +10,15 @@ from settings import SQL_BD_NAME
 
 
 def main(args):
+    #from datetime import datetime
+    #print((datetime.now() - datetime(year=2024, month=5, day=30, hour=20, minute=20)).total_seconds())
+
     update_env()
     if not exists(SQL_BD_NAME):
         create_bd()
     bot = Bot()
-
-    
     bot.run(get_env("BOT_TOKEN"))
+    
 
 
 if __name__ == "__main__":
