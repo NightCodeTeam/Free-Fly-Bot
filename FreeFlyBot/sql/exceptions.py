1bd81b6e73cfad6e6a58dcc1a55c23ffef3c91d0
Maxim Bodrov <activer12@mail.ru>
2024-04-05 13:51:09 +0300

    fix: dataclasses

--- FreeFlyBot/sql/exceptions.py
+++ FreeFlyBot/sql/exceptions.py
@@ -8,3 +8,9 @@ class SQLFullException(Exception):
     def __init__(self, table):
         self.txt = f'SQL TOO MANY ENTRIES {table}'
         super().__init__(self.txt)
+
+
+class SQLBadDataclassException(Exception):
+    def __init__(self, query):
+        self.txt = f'Try to create BAD dataclass with sql query {query}'
+        super().__init__(self.txt)
